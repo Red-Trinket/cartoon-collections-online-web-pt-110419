bf3005c9304446513b840a4bb8a29d74f1b92806
Matthew Jones <matts3371@gmail.com>
2019-11-14 15:43:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   i = 0
   while i < dwarves.length 
-    i += 1 
     puts "#{i+1}. #{dwarves[i]}"
+    i += 1 
   end 
 end
 
