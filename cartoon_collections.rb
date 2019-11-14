693f7baa5c964deeb12b1d761464cbdfa18a2540
Matthew Jones <matts3371@gmail.com>
2019-11-14 16:01:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,13 @@ def long_planeteer_calls(words)# code an argument here
   # Your code here
   words.any? do |call|
     call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese| 
+    array.include?(cheese)
+  end 
 end
