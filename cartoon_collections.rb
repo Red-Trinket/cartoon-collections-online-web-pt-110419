ef936066456e9436c8a99137f1c919820cb33888
Matthew Jones <matts3371@gmail.com>
2019-11-14 15:48:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,9 @@ def summon_captain_planet(array)# code an argument here
   array.collect {|name| name + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)# code an argument here
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
